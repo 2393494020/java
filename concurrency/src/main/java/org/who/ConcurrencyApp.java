512be97c7029e9c278c1c491974a50fd09cb2af8
2393494020 <2393494020@qq.com>
2019-04-03 18:05:30 +0800

    hello

--- concurrency/src/main/java/org/who/ConcurrencyApp.java
+++ concurrency/src/main/java/org/who/ConcurrencyApp.java
@@ -4,26 +4,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class ConcurrencyApp {
     private static Logger logger = LoggerFactory.getLogger(ConcurrencyApp.class);
     private AtomicInteger atomCount = new AtomicInteger(0);
     private int count = 0;
-    private ReentrantLock lock = new ReentrantLock(true);
+    private volatile boolean add = true;
+    // private ReentrantLock lock = new ReentrantLock(true);
 
-    private /*synchronized*/ void increase() {
-        for (int i = 1; i <= 10; i++) {
-            lock.lock();
-            count++;
-            lock.unlock();
-            try {
-                Thread.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            logger.info(Thread.currentThread().getName() + ":" + count);
-        }
+    private int getCount() {
+        return count;
+    }
+
+    private void increase() {
+        count++;
+        logger.info(Thread.currentThread().getName() + ":" + count);
+//        for (int i = 1; i <= 10; i++) {
+//            count++;
+//            try {
+//                Thread.sleep(5);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            logger.info(Thread.currentThread().getName() + ":" + count);
+//        }
     }
 
     private void increaseAtom() {
@@ -38,11 +42,33 @@ public class ConcurrencyApp {
     }
 
     public static void main(String[] args) {
-        ConcurrencyApp app = new ConcurrencyApp();
-        Thread[] threads = new Thread[10];
-        for (int i = 0; i < threads.length; i++) {
-            threads[i] = new Thread(app::increase);
-            threads[i].start();
+        final ConcurrencyApp app = new ConcurrencyApp();
+//        Thread[] threads = new Thread[100];
+//        for (int i = 0; i < threads.length; i++) {
+//            threads[i] = new Thread(app::increase);
+//            threads[i].start();
+//        }
+
+        new Thread(() -> {
+            while (app.add)
+                app.increase();
+        }).start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(50);
+                app.add = false;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        try {
+            Thread.sleep(45);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        logger.info(Thread.currentThread().getName() + ":" + app.getCount());
     }
 }
