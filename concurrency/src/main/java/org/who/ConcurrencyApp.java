d7dac69d177b8b334c51f8550b7d976040b96799
2393494020 <2393494020@qq.com>
2019-04-03 12:40:33 +0800

    hello

--- concurrency/src/main/java/org/who/ConcurrencyApp.java
+++ concurrency/src/main/java/org/who/ConcurrencyApp.java
@@ -1,24 +1,29 @@
 package org.who;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class CurrencyApp {
-    private AtomicInteger count = new AtomicInteger(0);
+public class ConcurrencyApp {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    // private AtomicInteger count = new AtomicInteger(0);
+    private int count;
 
     private /*synchronized*/ void increace() {
         for (int i = 1; i <= 10; i++) {
-            // count.incrementAndGet();
+            count++;
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(Thread.currentThread().getName() + ":" + count.incrementAndGet());
+            logger.info(Thread.currentThread().getName() + ":" + count);
         }
     }
 
     public static void main(String[] args) {
-        CurrencyApp app = new CurrencyApp();
+        ConcurrencyApp app = new ConcurrencyApp();
         Thread threads[] = new Thread[5];
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new Thread(new Runnable() {
