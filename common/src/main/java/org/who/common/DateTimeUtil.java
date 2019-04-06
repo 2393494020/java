0d735866d3596f43463a2a913d657bb65bb1dcbe
2393494020 <2393494020@qq.com>
2019-04-06 10:05:36 +0800

    hello

--- common/src/main/java/org/who/common/DateTimeUtil.java
+++
@@ -1,9 +0,0 @@
-package org.who.common;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class DateTimeUtil {
-    private static Logger logger = LoggerFactory.getLogger(DateTimeUtil.class);
-
-}
