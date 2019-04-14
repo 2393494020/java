b240b9f212ecd3b7034dcfc347ca6cb122ca3c89
2393494020 <2393494020@qq.com>
2019-04-15 07:21:00 +0800

    hello

---
+++ concurrency/src/main/java/org/who/TicketSeller.java
@@ -0,0 +1,31 @@
+package org.who;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class TicketSeller {
+    static Queue<String> ticketQueue = new ConcurrentLinkedQueue<>();
+
+    static {
+        for (int i = 1; i <= 10000; i++) {
+            ticketQueue.add("票 - " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String ticket = ticketQueue.poll();
+                        if (ticket == null)
+                            break;
+                        else
+                            System.out.println(Thread.currentThread().getName() + " - 卖出 - " + ticket);
+                    }
+                }
+            }).start();
+        }
+    }
+}
